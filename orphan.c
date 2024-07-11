0952a24bc6ca0d90a3629d1f0579462b15e222d8
labs-cs3 <166833716+labs-cs3@users.noreply.github.com>
2024-07-11 23:48:26 +0530

    Add files via upload

---
+++ orphan.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    int pid;
+    pid = fork();
+
+    if (pid < 0) {
+        printf("fork failed\n");
+    } else if (pid == 0) {
+        sleep(20);
+        printf("This is the child process with PID %d\n", getpid());
+        printf("Parent process ID for the child is %d\n", getppid());
+    } else {
+        printf("This is the parent process with PID %d\n", getpid());
+    }
+    return 0;
+}
